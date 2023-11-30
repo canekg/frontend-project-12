12a204a7c9fe8f28bf269962094c3e2ede21bbb0
Alexandr <canekg@mail.ru>
2023-12-01 01:00:36 +0500

    addfrontend/src/locales/

---
+++ frontend/src/locales/ru.js
@@ -0,0 +1,25 @@
+export default {
+  translation: {
+    loginPage: {
+      username: 'Ваш ник',
+      password: 'Пароль',
+      enter: 'Войти',
+      noAcc: 'Нет аккаунта?',
+      reg: 'Регистрация',
+      usernamePlaceholder: 'Ваш ник',
+      passwordPlaceholder: 'Пароль',
+      errors: {
+        networkError: 'Ошибка соединения',
+        authError: 'Неверные имя пользователя или пароль',
+      },
+    },
+    emptyPage: {
+      emptyPage: 'Страница не найдена',
+      pageLink1: 'Но вы можете перейти',
+      pageLink2: 'на главную страницу',
+    },
+    navigation: {
+      logout: 'Выйти',
+    },
+  },
+};
