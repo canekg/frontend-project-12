27eed139aae81b931263b99a6ed5ba4e4331aed9
Alexandr <canekg@mail.ru>
2023-12-01 01:02:48 +0500

    modified:   frontend/src/App.js and frontend/src/index.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable implicit-arrow-linebreak */
+import './index.css';
+import 'react-toastify/dist/ReactToastify.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+// import React, { useContext } from 'react';
+// import { useDispatch } from 'react-redux';
+// import { addMessage } from '../slices/messageSlice.js';
+// import { addChannel, renameChannel, removeChannel } from '../slices/channelsSlice.js';
+import Main from './Components/pages/Main.jsx';
+import Login from './Components/pages/Login.jsx';
+import Empty from './Components/pages/Empty.jsx';
+import Signup from './Components/pages/Signup.jsx';
+import routes from './routes';
+// import { SocketContext } from '../context/index';
+// import avatar from './assets/avatar.jpg';
 
-const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit
-        {' '}
-        <code>src/App.js</code>
-        {' '}
-        and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
-  </div>
-);
-
+const App = () =>
+  // const socket = useContext(SocketContext);
+  // const dispatch = useDispatch();
+  // socket.on('newMessage', (message) => {
+  //   dispatch(addMessage(message));
+  // });
+  // socket.on('newChannel', (channel) => {
+  //   dispatch(addChannel(channel));
+  // });
+  // socket.on('renameChannel', (channel) => {
+  //   dispatch(renameChannel({ id: channel.id, changes: channel }));
+  // });
+  // socket.on('removeChannel', (data) => {
+  //   dispatch(removeChannel(data.id));
+  // });
+  (
+    <>
+      <div className="d-flex flex-column h-100">
+        <BrowserRouter>
+          <Routes>
+            <Route path={routes.mainPage} element={<Main />} />
+            <Route path={routes.loginPage} element={<Login />} />
+            <Route path={routes.signupPage} element={<Signup />} />
+            <Route path="*" element={<Empty />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+      <ToastContainer />
+    </>
+  );
 export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Provider, ErrorBoundary } from '@rollbar/react';
+import init from './init.jsx';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+const app = async () => {
+  const rollbarConfig = {
+    accessToken: process.env.REACT_APP_NOT_SECRET_TOKEN,
+    environment: 'production',
+  };
+  const root = ReactDOM.createRoot(document.getElementById('chat'));
+  root.render(
+    <Provider config={rollbarConfig}>
+      <ErrorBoundary>
+        {await init()}
+      </ErrorBoundary>
+    </Provider>,
+  );
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+app();
