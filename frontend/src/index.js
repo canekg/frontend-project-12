557b46003eacaf2a4d5660b0a353627f0736ffa8
Alexandr <canekg@mail.ru>
2023-12-06 23:15:17 +0500

    modified:   frontend/src/init.jsx, frontend/src/index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,23 +1,10 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import { Provider, ErrorBoundary } from '@rollbar/react';
 import init from './init.jsx';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 const app = async () => {
-  const rollbarConfig = {
-    accessToken: process.env.REACT_APP_NOT_SECRET_TOKEN,
-    environment: 'production',
-  };
   const root = ReactDOM.createRoot(document.getElementById('chat'));
-  root.render(
-    <Provider config={rollbarConfig}>
-      <ErrorBoundary>
-        {await init()}
-      </ErrorBoundary>
-    </Provider>,
-  );
+  root.render(await init());
 };
 
 app();
--- frontend/src/init.jsx
+++ frontend/src/init.jsx
@@ -1,23 +1,50 @@
-/* eslint-disable no-unused-vars */
 import i18next from 'i18next';
 import { I18nextProvider, initReactI18next } from 'react-i18next';
 import { Provider } from 'react-redux';
-import React from 'react';
-import App from './App.js';
+import io from 'socket.io-client';
+import { Provider as RollbarProvider, ErrorBoundary } from '@rollbar/react';
+import App from './Components/App.js';
 import resources from './locales/index.js';
+import AuthProvider from './context/AuthProvider.jsx';
+import SocketProvaider from './context/SocketProvider.jsx';
+import store from './slices/index.js';
+import rollbarConfig from './rollbarConfig.js';
+import FilterProvider from './context/FilterProvider.jsx';
+import { addMessages } from './slices/messagesSlice';
+import {
+  addChannel, removeChanneFromState, renameChannelFromState,
+} from './slices/channelsSlice';
 
 const init = async () => {
   const i18n = i18next.createInstance();
-  await i18n
-    .use(initReactI18next)
-    .init({
-      resources,
-      fallbackLng: 'ru',
-    });
+  const socket = io();
+
+  socket.on('newMessage', (payload) => store.dispatch(addMessages(payload)));
+  socket.on('newChannel', (payload) => store.dispatch(addChannel(payload)));
+  socket.on('removeChannel', (payload) => store.dispatch(removeChanneFromState(payload)));
+  socket.on('renameChannel', (payload) => store.dispatch(renameChannelFromState(payload)));
+
+  await i18n.use(initReactI18next).init({
+    resources,
+    fallbackLng: 'ru',
+  });
+
   return (
-    <I18nextProvider i18n={i18n}>
-      <App />
-    </I18nextProvider>
+    <RollbarProvider config={rollbarConfig}>
+      <ErrorBoundary>
+        <Provider store={store}>
+          <AuthProvider>
+            <SocketProvaider socket={socket}>
+              <FilterProvider>
+                <I18nextProvider i18n={i18n}>
+                  <App />
+                </I18nextProvider>
+              </FilterProvider>
+            </SocketProvaider>
+          </AuthProvider>
+        </Provider>
+      </ErrorBoundary>
+    </RollbarProvider>
   );
 };
 
