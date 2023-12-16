cb70493a2b55bfda6eff1282be162e4ee30dba74
Alexandr <canekg@mail.ru>
2023-12-16 15:12:32 +0500

    updated accessToken in frontend/src/rollbarConfig.js

--- frontend/src/rollbarConfig.js
+++ frontend/src/rollbarConfig.js
@@ -1,5 +1,5 @@
 const rollbarConfig = {
-  accessToken: '9e7b5b5910e040f7ab1e662c56cdedf9',
+  accessToken: process.env.ROLLBAR_KEY,
   captureUncaught: true,
   captureUnhandledRejections: true,
   environment: 'production',
